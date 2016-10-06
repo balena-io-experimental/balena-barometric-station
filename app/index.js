5f832f7ce4efbf3e7763add54db61f6eaea21696
Carlo Maria Curinga <Carlo Maria Curinga>
2016-10-06 12:54:14 +0200

    added temp handling on LCD

--- app/index.js
+++ app/index.js
@@ -7,8 +7,11 @@
     const chip = (process.env.BAROMETER_CHIP == null) ? 'bmp280' : process.env.BAROMETER_CHIP;
     const barometer = require(__dirname + '/libs/' + chip + '.js');
     let currentVolume = 0;
+    let color = {};
     lcd.init();
     barometer.start();
+
+    lcd.writeOnDisplay(0, 0, 'Resin.io Barometer');
 
     lcd.on('button', (btn) => {
         console.log('A button has been pressed: ', btn);
@@ -32,11 +35,69 @@
         }
     });
 
-    barometer.on('error',(err) => {
-      console.log(err);
+    barometer.on('error', (err) => {
+        console.log(err);
     });
 
+    let getColorByTemp = function(temp) {
+        let tempInt = parseInt(temp);
+        switch (true) {
+            case (tempInt < 16):
+                color = {
+                    r: 70,
+                    g: 69,
+                    b: 244
+                };
+                break;
+            case (tempInt >= 16 && tempInt < 19):
+                color = {
+                    r: 105,
+                    g: 69,
+                    b: 244
+                };
+                break;
+            case (tempInt >= 19 && tempInt < 22):
+                color = {
+                    r: 140,
+                    g: 69,
+                    b: 244
+                };
+                break;
+            case (tempInt >= 22 && tempInt < 25):
+                color = {
+                    r: 175,
+                    g: 69,
+                    b: 244
+                };
+                break;
+            case (tempInt >= 25 && tempInt < 28):
+                color = {
+                    r: 244,
+                    g: 69,
+                    b: 229
+                };
+                break;
+            case (tempInt >= 28 && tempInt < 31):
+                color = {
+                    r: 244,
+                    g: 69,
+                    b: 183
+                };
+                break;
+            case (tempInt >= 31):
+                color = {
+                    r: 254,
+                    g: 46,
+                    b: 46
+                };
+                break;
+        }
+        return color;
+    };
+
     barometer.on('reading', (data) => {
-      console.log('pressure: ' + data.pressure + '; temperature: ' + data.temperature);
+        let colorTemp = getColorByTemp(data.temperature);
+        lcd.setColor(colorTemp.r,colorTemp.g,colorTemp.b);
+        lcd.writeOnDisplay(0, 1, 'T:' + data.temperature + ' P:' + data.pressure);
     });
 }
